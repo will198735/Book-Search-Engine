3a0ab64139275831291a12001c5096badaca6383
will198735 <recinoswilfredo@gmail.com>
2023-04-05 08:53:06 -0700

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+const { resolvers,typeDefs } = require('./schemas');
+
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +25,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
